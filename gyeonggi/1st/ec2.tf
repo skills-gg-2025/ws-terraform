43cbe5b2705576074e3c9222d6d5e8f34a5784cd
cloudsense2025 <mcblueboy2006@gmail.com>
2025-07-27 10:26:42 +0900

    gyeongbuk 2nd CDN src upload, amazon linux 2023 data fix

--- gyeonggi/1st/ec2.tf
+++ gyeonggi/1st/ec2.tf
@@ -5,7 +5,22 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-*-x86_64"]
+    values = ["al2023-ami-2023.*-kernel-*-x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
   }
 }
 
